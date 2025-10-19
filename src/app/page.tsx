2f94167bc58988ecbe1a608e664616149b3ad998
Taj-786 <tajuddinshaik786r@gmail.com>
2025-10-19 21:31:05 +0530

    Added Theme Context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import Image from "next/image";
+"use client";
+
+import ToggleSwitch from "@/components/ToggleSwitch";
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-between items-center bg-primary min-h-screen ">
+    <div className="flex flex-row justify-between items-center bg-primary-light dark:bg-primary-dark min-h-screen">
       <div className="w-[15%] min-h-screen">
       </div>
 
-      <div className="bg-right-nav w-[85%] m-2 min-h-[98vh] rounded-2xl flex flex-row overflow-hidden">
-        <div className="w-[20%] bg-left-nav"></div>
-        <div className="w-[80%] bg-right-nav"></div>
+      <div className="w-[85%] m-2 min-h-[98vh] rounded-2xl flex flex-row overflow-hidden bg-left-nav-light dark:bg-left-nav-dark">
+        <div className="w-[20%]"></div>
+        <div className="w-[80%] bg-right-nav-light dark:bg-right-nav-dark">
+          <div className="flex justify-end items-end m-2">
+            <ToggleSwitch />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ToggleSwitch.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
+
+export default function ToggleSwitch() {
+    const { setTheme, resolvedTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="text-black dark:text-white hover:opacity-75 transition-opacity"
+            onClick={() =>
+                setTheme(resolvedTheme === "dark" ? "light" : "dark")
+            }
+        >
+            {resolvedTheme === "dark" ? <FiSun size={30}/> : <FiMoon size={30}/>}
+        </button>
+    )
+}
