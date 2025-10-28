b1053dbcedc46b804c61946009b228cca8ef020b
Taj-786 <tajuddinshaik786r@gmail.com>
2025-10-28 15:54:16 +0530

    Fixed the Production Errors - II

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
 import ToggleSwitch from "@/components/ToggleSwitch";
 import Profile from "@/components/home/Profile";
 import Ads from "@/components/Ads";
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
 import ToggleSwitch from "@/components/ToggleSwitch";
 import Profile from "@/components/search/Profile";
 import SearchBar from "@/components/search/SearchBar";
