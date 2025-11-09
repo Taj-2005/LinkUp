30b6a9acf72d56f87681800b7c3ef45d0801829d
Taj-786 <tajuddinshaik786r@gmail.com>
2025-11-09 17:03:44 +0530

    fixed bugs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://linkup-7ycv.onrender.com/api/:path*",
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
